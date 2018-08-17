f2b7ca68540791d2be3a6cc165b94bb2fc371dbb
René Sprotte <renspr@gmail.com>
2018-08-17 10:56:52 +0200

    Share master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, "git@github.com:ubpb/schulungs_db.git"
 set :branch, "master"
 set :log_level, :debug
 
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 set :rvm_type, :user
