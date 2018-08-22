a3dd6a68186aa08eb1580f28fb7796cf407c028c
René Sprotte <renspr@gmail.com>
2018-08-22 14:13:18 +0200

    Add task to pull the remote db.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,3 +11,60 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 set :rvm_type, :user
 set :rvm_ruby_version, "2.5.1"
 set :rails_env, "production"
+
+namespace :app do
+  namespace :db do
+    desc "Pull db from remote server and install locally"
+    task :pull do
+      # Find the first server in role 'db' (all db servers read the same database)
+      server = Capistrano::Configuration.env.send(:servers).find{ |s| s.roles.include?(:db) }
+      raise "No proper server found" if server.nil?
+
+      # Setup variables
+      dump_file_name   = "#{fetch(:application)}-#{Time.now.strftime("%Y%m%d-%H%M%S")}.dump"
+      remote_dump_file = "/tmp/#{dump_file_name}"
+      local_dump_file  = "/tmp/#{dump_file_name}"
+
+      # Dump db on remote server
+      on(server) do |host|
+        db_config = YAML.load(capture("cat #{shared_path}/config/database.yml"))[fetch(:rails_env)]
+
+        host     = db_config["host"]
+        database = db_config["database"]
+        username = db_config["username"]
+        password = db_config["password"]
+
+        execute("mysqldump -h #{host} -u #{username} -p#{password} -r #{remote_dump_file} #{database}")
+      end
+
+      # Download file
+      on(server) do |host|
+        download!(remote_dump_file, local_dump_file)
+      end
+
+      # Restore dump locally
+      run_locally do
+        db_config = YAML.load(capture(:cat, "config/database.yml"))["development"]
+
+        host     = db_config["host"] || "localhost"
+        database = db_config["database"]
+        username = db_config["username"]
+        password = db_config["password"]
+
+        password_param = password ? "-p#{password}" : ""
+
+        execute("mysql -h #{host} -u #{username} #{password_param} #{database} < #{local_dump_file}")
+      end
+
+      # Delete dump on remote server
+      on(server) do |host|
+        execute("rm #{remote_dump_file}")
+      end
+
+      # Delete dump locally
+      run_locally do
+        execute("rm #{local_dump_file}")
+      end
+    end
+  end
+end
