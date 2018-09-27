e00ccc3f17f7f18022550e69dbe6af07ed118677
René Sprotte <renspr@gmail.com>
2018-09-27 11:02:27 +0200

    Remove incompatable default value.

--- db/migrate/20180925114121_add_reminder_message_sent_at_to_registration.rb
+++ db/migrate/20180925114121_add_reminder_message_sent_at_to_registration.rb
@@ -1,5 +1,5 @@
 class AddReminderMessageSentAtToRegistration < ActiveRecord::Migration[5.2]
   def change
-    add_column :registrations, :sent_reminder_message_at, :timestamp, null: true, default: nil
+    add_column :registrations, :sent_reminder_message_at, :timestamp, null: true
   end
 end
