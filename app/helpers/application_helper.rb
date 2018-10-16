d0b8e61121e25d729e1a64d9333fd700bd754432
René Sprotte <renspr@gmail.com>
2018-10-16 11:28:45 +0200

    Fix date to words routine.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,13 @@ module ApplicationHelper
     GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, s)
   end
 
+  def date_in_words(date)
+    case date
+    when Date.today          then t("datetime.today")
+    when Date.today + 1.day  then t("datetime.tomorrow")
+    when Date.today + 2.days then t("datetime.day_after_tomorrow")
+    else distance_of_time_in_words_to_now(date, scope: "datetime.distance_in_words.date_only")
+    end
+  end
+
 end
