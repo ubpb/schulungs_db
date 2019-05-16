19b5e09d3756f15e83ce0e6c9b8d1b8ea8bfbcd6
René Sprotte <renspr@gmail.com>
2019-05-16 14:00:31 +0200

    Reorganizing flash messages.

--- app/controllers/frontend/registrations_controller.rb
+++ app/controllers/frontend/registrations_controller.rb
@@ -4,13 +4,11 @@ class Frontend::RegistrationsController < Frontend::ApplicationController
     @training_course = TrainingCourse.published.upcoming.find(params[:training_course_id])
     @registration = @training_course.registrations.build
 
-    set_breadcrumbs
-
-    set_page_title(t("frontend.page_titles.registrations.new"))
+    set_breadcrumbs_and_page_title
 
     verify_training_course or return
   rescue ActiveRecord::RecordNotFound
-    flash[:error] = t(".flash.error")
+    flash[:error] = t("frontend.registrations.flash.error")
     redirect_to frontend_training_courses_path
   end
 
@@ -18,12 +16,12 @@ class Frontend::RegistrationsController < Frontend::ApplicationController
     @training_course = TrainingCourse.published.upcoming.find(params[:training_course_id])
     @registration = @training_course.registrations.build(registration_params)
 
-    set_breadcrumbs
+    set_breadcrumbs_and_page_title
 
     verify_training_course or return
 
     if @registration.save
-      flash[:success] = t(".flash.success", email: @registration.email)
+      flash[:success] = t("frontend.registrations.flash.success", email: @registration.email)
       Mailers::RegistrationsMailer.registration_confirmation(@registration).deliver
       Mailers::RegistrationsMailer.registration_notification(@registration).deliver
 
@@ -42,20 +40,22 @@ class Frontend::RegistrationsController < Frontend::ApplicationController
 
 private
 
-  def set_breadcrumbs
+  def set_breadcrumbs_and_page_title
     add_breadcrumb(t("frontend.breadcrumbs.training_courses.index"), frontend_training_courses_path)
     add_breadcrumb(t("frontend.breadcrumbs.training_courses.show", title: @training_course.title), frontend_training_course_path(@training_course))
     add_breadcrumb(t("frontend.breadcrumbs.registrations.new"), new_frontend_training_course_registration_path(@training_course))
+
+    set_page_title(t("frontend.page_titles.registrations.new"))
   end
 
   def verify_training_course
     unless @training_course.registration_required?
-      flash[:error] = t(".flash.no_registration_error")
+      flash[:error] = t("frontend.registrations.flash.no_registration_error")
       redirect_to frontend_training_course_path(@training_course) and return
     end
 
     if @training_course.full?
-      flash[:error] = t(".flash.full_error")
+      flash[:error] = t("frontend.registrations.flash.full_error")
       redirect_to frontend_training_course_path(@training_course) and return
     end
 
