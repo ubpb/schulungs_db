d37265d7fec2e2b017f1835e22cc6c2a212ec549
René Sprotte <renspr@users.noreply.github.com>
2023-04-26 14:43:37 +0200

    #add_template_helper was removed in Rails 6.1

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: "schulung@ub.uni-paderborn.de"
   default reply_to: "schulung@ub.uni-paderborn.de"
   layout "mailer"
-  add_template_helper(MailerHelper)
+  helper MailerHelper
 end
