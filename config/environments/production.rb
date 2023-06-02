8b0f9375915ee10a93825aeacd384b51862d7ad2
zimmp <121883156+zimmp@users.noreply.github.com>
2023-06-02 16:07:00 +0200

    bugfix setting openssl_verify_mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "schulungs_db_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = { openssl_verify_mode: OpenSSL::SSL::VERIFY_NONE }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
