47bc7b7a765c2910a65126f91f9f447d3262b440
René Sprotte <renspr@users.noreply.github.com>
2024-04-02 11:21:43 +0200

    Use sendmail in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Send mails using sendmail
+  config.action_mailer.delivery_method = :sendmail
 end
